ab9365a07b51155944acaf3d409e30f9618ca96c
FranklinPerez13 <75079852+FranklinPerez13@users.noreply.github.com>
2021-02-26 21:10:30 -0400

    Add files via upload

---
+++ Practica 3/Proyecto División de Dos Números/Division de Numeros.js
@@ -0,0 +1,11 @@
+var a = prompt("Ingrese el primer numero")
+var b = prompt("Ingrese el segundo numero")
+
+if (b == 0)
+{
+ document.write("No se puede dividir entre cero 0 ");
+} 
+else
+{
+    document.write("El resueltado es: " + a/b);
+}
